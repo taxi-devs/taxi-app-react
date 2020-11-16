9c32885b5934201917459700c7843a60cab2c5ee
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-16 12:55:33 +1000

    update App.js. add imports for components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Body from './Components/Body'
+import Footer from './Components/Footer'
 
 function App() {
   return (
