3ffee62559c2ed1479d7bfc66140173c9da2e546
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-18 10:36:27 +1000

    update slider. use react-elastic-carousel package

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,11 @@
-import Slider from './Slider'
-import CarImages from './SlideImages'
+import Slide from './Slide'
 
 const Home = () => {
     return (
         <div className="body-page">
-            <Slider slides={ CarImages } />
+            <Slide />
             <h2>Need a Ride from a Trusted Service Provider???</h2>
             <q>Check Us Out</q>
-
             <button>BOOK NOW</button>
         </div>
     );
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,16 +1,28 @@
-import { css } from '@emotion/react'
+import React, { Component } from 'react'
+import Carousel from 'react-elastic-carousel'
 
-const Slide = ({ content, width }) => (
-    <div
-        css={css`
-      height: 100%;
-      width: ${width}px;
-      background-image: url('${content}');
-      background-size: cover;
-      background-repeat: no-repeat;
-      background-position: center;
-    `}
-    />
-)
+const breakpoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 2000, itemsToShow: 2 },
+    { width: 4000, itemsToShow: 3 },
+]
 
-export default Slide
+export default class Slide extends Component {
+    state = {
+        items: [
+            { id: 1, url: 'https://i.ibb.co/ZmV7srF/markii.jpg', alt: 'mark ii' },
+            { id: 2, url: 'https://i.ibb.co/y64BsbD/premio.jpg', alt: 'premio' },
+            { id: 3, url: 'https://i.ibb.co/HrTn6z9/xtrail.jpg', alt: 'xtrail' }
+        ]
+    }
+    render() {
+        const { items } = this.state;
+        return (
+            <>
+                <Carousel enableAutoPlay autoPlaySpeed={6000} breakPoints={ breakpoints } >
+                    {items.map(item => <div key={ item.id }> <img src={ item.url } alt={ item.alt } /></div>)}
+                </Carousel>
+            </>
+        )
+    }
+}
