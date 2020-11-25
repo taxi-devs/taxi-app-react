1d40c7c1f13eef67c83c122d2945b9de2543d12b
Frank Manue Jr <fpj.manue.jnr@hotmail.com>
2020-11-25 12:49:29 +1000

    Update Home.jsx
    add button click event for book now

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,14 @@
 import Slide from "./Slide";
+import { useHistory } from 'react-router-dom'
+
 
 const Home = ({ carItems }) => {
+  const history = useHistory();
+  
+  const Book = () => {
+    history.push('/booking-form')
+  }
+
   return (
     <div className="body-page">
       <h2 style={{ margin: 5 }}>
@@ -10,7 +18,7 @@ const Home = ({ carItems }) => {
         <q>Check Us Out</q>
       </h4>
       <Slide carItems={carItems} />
-      <button style={{ padding: 10, fontWeight: "bold", fontSize: 20 }}>
+      <button style={{ padding: 10, fontWeight: "bold", fontSize: 20 }} onClick={ Book }>
         BOOK NOW
       </button>
     </div>
